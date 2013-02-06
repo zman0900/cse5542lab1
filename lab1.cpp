c709ec169e7e4c57298a3ac7ce0b50d8624491d1
Dan Ziemba <zman0900@gmail.com>
2013-02-06 03:54:03 -0500

    Finished help

--- lab1.cpp
+++ lab1.cpp
@@ -12,6 +12,14 @@ int main(int argc, char *argv[]) {
 	cout << "\t'a' - zoom in" << endl;
 	cout << "\t'z' - zoom out" << endl;
 	cout << "\t'q' - reset pan and zoom" << endl;
+	cout << "\t'f' - add vertex to current polyline" << endl;
+	cout << "\t't' - rotate last vertex by 10 degrees" << endl;
+	cout << "\t'S' - enlarge last vertex by 50%" << endl;
+	cout << "\t's' - shrink last vertex by 50%" << endl;
+	cout << "\t'p' - draw polygon" << endl;
+	cout << "\t'n' - draw star" << endl;
+	cout << "\t'r' - draw spiral" << endl;
+	cout << "\tclick - start new ployline at mouse" << endl;
 
 	GlGlut g;
 	g.start(&argc, argv);
